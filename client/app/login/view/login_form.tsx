2076d0a65b9f1c63215dd6885bb5f53201e838e3
Samuel Manik <79486973+Pluvas159@users.noreply.github.com>
2024-04-22 20:56:09 +0200

    init with login

---
+++ client/app/login/view/login_form.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string(),
+});
+
+export default function LoginForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  //   <Card>
+
+  //   <CardContent>
+  //     <p>Card Content</p>
+  //   </CardContent>
+  //   <CardFooter>
+  //     <p>Card Footer</p>
+  //   </CardFooter>
+  // </Card>
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Prihlásenie</CardTitle>
+        {/* <CardDescription>Card Description</CardDescription> */}
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input size={40} placeholder="shadcn" {...field} />
+                  </FormControl>
+                  {/* <FormDescription>Email vašeho účtu</FormDescription> */}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Heslo</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" type="password" {...field} />
+                  </FormControl>
+                  {/* <FormDescription>Vaše </FormDescription> */}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Prihlásiť</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ client/app/page.tsx
@@ -0,0 +1,16 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="absolute top-2  right-2 flex flex-row gap-1 ">
+        <ModeToggle />
+      </div>
+      <Link href={"/login"}>
+        <Button>Prihlásiť</Button>
+      </Link>
+    </main>
+  );
+}
